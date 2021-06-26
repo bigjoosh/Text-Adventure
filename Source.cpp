98c7de3c5223f1d0c3d4cbab902b3a7d979e6338
bigjoosh <85456042+bigjoosh@users.noreply.github.com>
2021-06-26 10:52:46 -0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -29,6 +29,7 @@ choiceOne:
 		goto choiceTwo;
 	}
 	else if (choiceOne == 2) {
+		cout << "-------------------------------------------" << endl;
 		cout << "\nYou sit on your butt the entire year!" << endl;
 		goto choiceThree;
 	}
@@ -45,11 +46,13 @@ choiceTwo:
 	cin >> choiceTwo;
 
 	if (choiceTwo == 1) {
+		cout << "-------------------------------------------" << endl;
 		cout << "You return home fearing starvation!" << endl;
-
+		goto choiceFour;
 	}
 	else if (choiceTwo == 2) {
 		cout << "You continue despite hunger!" << endl;
+		goto choiceFive;
 	}
 	else {
 		cout << "Please enter '1' or '2'!";
@@ -57,8 +60,45 @@ choiceTwo:
 	}
 choiceThree: //ending 1
 	int choiceThree;
+	cout << "-------------------------------------------" << endl;
 	cout << "This was a huge mistake because, little did you know, an enemy army was training hard and wipped out your kingdom without struggle!" << endl;
 	cout << "~~~~~~~~~~~~~~~~~~~~~GAME OVER~~~~~~~~~~~~~~~~~~~~~" << endl;
-	cout << "Ending 1/1";
+	cout << "Ending 1/3";
 		_getch();
+		return 1;
+
+	choiceFour:
+		int choiceFour;
+		cout << "Your soldiers are happy and well fed!" << endl;
+		cout << "The king has ordered you to attack a neighboring nation!" << endl;
+		cout << "How do you wish to proceed?" << endl;
+		cout << "Enter '1' to attack from the front?" << endl;
+		cout << "Enter '2' to attack both flanks?" << endl;
+		cin >> choiceFour;
+		
+		if (choiceFour == 1) {
+			//ending 3
+			cout << "They defeated you and your army!" << endl;
+			cout << "~~~~~~~~~~~~~~~~~~~~~GAME OVER~~~~~~~~~~~~~~~~~~~~~" << endl;
+			cout << "Ending 3/3";
+			_getch();
+			return 3;
+		}
+		else if (choiceFour == 2) {
+			cout << "Congrats, you win!!" << endl;
+			return 4;
+		}
+		else {
+			cout << "Please enter '1' or '2'!" << endl;
+			goto choiceFour;
+		}
+
+	choiceFive: //ending 2
+		int choiceFive;
+		cout << "Your army has been wipped out by starvation and famine!" << endl;
+		cout << "~~~~~~~~~~~~~~~~~~~~~GAME OVER~~~~~~~~~~~~~~~~~~~~~" << endl;
+		cout << "Ending 2/3";
+		_getch();
+		return 2;
+		
 }
